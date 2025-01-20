95cf8e11f8bacc54cd531404867cad3e5450d742
devnouiq <dev@nouiq.com>
2025-01-20 13:16:13 +0530

    remove trace from function ck aws

--- helloworld/views.py
+++ helloworld/views.py
@@ -3,13 +3,13 @@ from datetime import datetime
 from hello.utils import save_time_to_db, perform_computation, fetch_from_database
 from hello.decorators import trace_function
 
-@trace_function()
+# @trace_function()
 def hello_world(request):
     result = perform_computation()
     data = fetch_from_database()
     return JsonResponse({"message": "Hello, World!", "result": result, "data": data})
 
-@trace_function()
+# @trace_function()
 def time_route(request):
     current_time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
     save_time_to_db(current_time)
